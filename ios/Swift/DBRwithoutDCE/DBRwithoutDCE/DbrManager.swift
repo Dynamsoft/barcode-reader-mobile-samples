d808c38fcabcef49b1dee7dff9d9a0e60d1a9d38
Amro Anwer <amro_anwer41@hotmail.com>
2022-02-24 13:44:03 -0800

    Added a new sample
    This sample showcases how to use DBR without the use of DCE. Currently using v8.8 of DBR and working to update the sample in v8.10.

---
+++ ios/Swift/DBRwithoutDCE/DBRwithoutDCE/DbrManager.swift
@@ -0,0 +1,261 @@
+//
+//  DbrManager.swift
+//  DBRwithoutDCE
+//
+//  Created by Amro Ibrahim on 2022-02-23.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+import DynamsoftBarcodeReader
+
+class DbrManager: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, DMDLSLicenseVerificationDelegate {
+    var barcodeFormat:Int?
+    var barcodeFormat2:Int?
+    var startRecognitionDate:NSDate?
+    var startVidioStreamDate:NSDate?
+    var isPauseFramesComing:Bool?
+    var isCurrentFrameDecodeFinished:Bool?
+    var adjustingFocus:Bool?
+    var m_videoCaptureSession:AVCaptureSession!
+    var barcodeReader: DynamsoftBarcodeReader!
+    var settings:iPublicRuntimeSettings?
+    var m_recognitionCallback:Selector?
+    var m_recognitionReceiver:ViewController?
+    var m_verificationCallback:Selector?
+    var m_verificationReceiver:ViewController?
+
+    var inputDevice:AVCaptureDevice?
+    var itrFocusFinish:Int!
+    var firstFocusFinish:Bool!
+    init(DLS:iDMDLSConnectionParameters) {
+        super.init()
+        barcodeReader = DynamsoftBarcodeReader(licenseFromDLS: DLS, verificationDelegate: self)
+        
+        self.parametersInit()
+    }
+    
+    init(license:String)
+    {
+        super.init()
+        barcodeReader = DynamsoftBarcodeReader(license: license)
+        
+        //Best Coverage settings
+        //barcodeReader.initRuntimeSettings(with: "{\"ImageParameter\":{\"Name\":\"BestCoverage\",\"DeblurLevel\":9,\"ExpectedBarcodesCount\":512,\"ScaleDownThreshold\":100000,\"LocalizationModes\":[{\"Mode\":\"LM_CONNECTED_BLOCKS\"},{\"Mode\":\"LM_SCAN_DIRECTLY\"},{\"Mode\":\"LM_STATISTICS\"},{\"Mode\":\"LM_LINES\"},{\"Mode\":\"LM_STATISTICS_MARKS\"}],\"GrayscaleTransformationModes\":[{\"Mode\":\"GTM_ORIGINAL\"},{\"Mode\":\"GTM_INVERTED\"}]}}", conflictMode: EnumConflictMode.overwrite, error: nil)
+        //Best Speed settings
+        //barcodeReader.initRuntimeSettings(with: "{\"ImageParameter\":{\"Name\":\"BestSpeed\",\"DeblurLevel\":3,\"ExpectedBarcodesCount\":512,\"LocalizationModes\":[{\"Mode\":\"LM_SCAN_DIRECTLY\"}],\"TextFilterModes\":[{\"MinImageDimension\":262144,\"Mode\":\"TFM_GENERAL_CONTOUR\"}]}}", conflictMode: EnumConflictMode.overwrite, error: nil)
+        //balance settings
+        barcodeReader.initRuntimeSettings(with: "{\"ImageParameter\":{\"Name\":\"Balance\",\"DeblurLevel\":5,\"ExpectedBarcodesCount\":512,\"LocalizationModes\":[{\"Mode\":\"LM_CONNECTED_BLOCKS\"},{\"Mode\":\"LM_SCAN_DIRECTLY\"}]}}", conflictMode: EnumConflictMode.overwrite, error:nil)
+        settings = try! barcodeReader.getRuntimeSettings()
+        settings!.barcodeFormatIds = Int(EnumBarcodeFormat.ONED.rawValue) | Int(EnumBarcodeFormat.PDF417.rawValue) | Int(EnumBarcodeFormat.QRCODE.rawValue) | Int(EnumBarcodeFormat.DATAMATRIX.rawValue)
+        settings!.barcodeFormatIds_2 = Int(EnumBarcodeFormat2.Null.rawValue)
+        barcodeReader.update(settings!, error: nil)
+        self.parametersInit()
+    }
+    
+    deinit {
+        barcodeReader = nil
+        if(m_videoCaptureSession != nil)
+        {
+            if(m_videoCaptureSession.isRunning)
+            {
+                m_videoCaptureSession.stopRunning()
+            }
+            m_videoCaptureSession = nil
+        }
+        inputDevice = nil
+        m_recognitionReceiver = nil
+        m_recognitionCallback = nil
+        m_verificationReceiver = nil
+        m_verificationCallback = nil
+    }
+    
+    func connectServerAfterInit(DLS:iDMDLSConnectionParameters)
+    {
+        barcodeReader = DynamsoftBarcodeReader(licenseFromDLS: DLS, verificationDelegate: self)
+    }
+    
+    func parametersInit()
+    {
+        m_videoCaptureSession = nil
+        isPauseFramesComing = false
+        isCurrentFrameDecodeFinished = true
+        barcodeFormat = Int(EnumBarcodeFormat.ONED.rawValue) | Int(EnumBarcodeFormat.PDF417.rawValue) | Int(EnumBarcodeFormat.QRCODE.rawValue) | Int(EnumBarcodeFormat.DATAMATRIX.rawValue)
+        barcodeFormat2 = 0
+        startRecognitionDate = nil
+        m_recognitionReceiver = nil
+        startVidioStreamDate  = NSDate()
+        adjustingFocus = true
+        itrFocusFinish = 0
+        firstFocusFinish = false
+    }
+    
+    func setBarcodeFormat(format:Int, format2:Int)
+    {
+        do
+        {
+            barcodeFormat = format
+            barcodeFormat2 = format2
+            let settings = try barcodeReader.getRuntimeSettings()
+            settings.barcodeFormatIds = format
+            settings.barcodeFormatIds_2 = format2
+            barcodeReader.update(settings, error: nil)
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    func setVideoSession()
+    {
+        do
+        {
+            inputDevice = self.getAvailableCamera()
+            let tInputDevice = inputDevice!
+            let captureInput = try? AVCaptureDeviceInput(device: tInputDevice)
+            let captureOutput = AVCaptureVideoDataOutput.init()
+            captureOutput.alwaysDiscardsLateVideoFrames = true
+            var queue:DispatchQueue
+            queue = DispatchQueue(label: "dbrCameraQueue")
+            captureOutput.setSampleBufferDelegate(self as AVCaptureVideoDataOutputSampleBufferDelegate, queue: queue)
+            
+            // Enable continuous autofocus
+            if(tInputDevice.isFocusModeSupported(AVCaptureDevice.FocusMode.continuousAutoFocus))
+            {
+                try tInputDevice.lockForConfiguration()
+                tInputDevice.focusMode = AVCaptureDevice.FocusMode.continuousAutoFocus
+                tInputDevice.unlockForConfiguration()
+            }
+            
+            // Enable AutoFocusRangeRestriction
+            if(tInputDevice.isAutoFocusRangeRestrictionSupported)
+            {
+                try tInputDevice.lockForConfiguration()
+                tInputDevice.autoFocusRangeRestriction = AVCaptureDevice.AutoFocusRangeRestriction.near
+                tInputDevice.unlockForConfiguration()
+            }
+            //kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange, kCVPixelFormatType_420YpCbCr8BiPlanarFullRange and kCVPixelFormatType_32BGRA
+            captureOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey : kCVPixelFormatType_32BGRA] as [String : Any]
+            
+            if(captureInput == nil)
+            {
+                return
+            }
+            self.m_videoCaptureSession = AVCaptureSession.init()
+            self.m_videoCaptureSession.addInput(captureInput!)
+            self.m_videoCaptureSession.addOutput(captureOutput)
+            
+            if(self.m_videoCaptureSession.canSetSessionPreset(AVCaptureSession.Preset(rawValue: "AVCaptureSessionPreset1920x1080"))){
+                self.m_videoCaptureSession.sessionPreset = AVCaptureSession.Preset(rawValue: "AVCaptureSessionPreset1920x1080")
+            }
+            else if(self.m_videoCaptureSession.canSetSessionPreset(AVCaptureSession.Preset(rawValue: "AVCaptureSessionPreset1280x720"))){
+                self.m_videoCaptureSession.sessionPreset = AVCaptureSession.Preset(rawValue: "AVCaptureSessionPreset1280x720")
+            }
+            else if(self.m_videoCaptureSession.canSetSessionPreset(AVCaptureSession.Preset(rawValue: "AVCaptureSessionPreset640x480"))){
+                self.m_videoCaptureSession.sessionPreset = AVCaptureSession.Preset(rawValue: "AVCaptureSessionPreset640x480")
+            }
+        }catch{
+            print(error)
+        }
+    }
+    
+    func startVideoSession()
+    {
+        if(!self.m_videoCaptureSession.isRunning)
+        {
+            self.m_videoCaptureSession.startRunning()
+        }
+    }
+    
+    func getAvailableCamera() -> AVCaptureDevice {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .unspecified
+        )
+        //let videoDevices = AVCaptureDevice.devices(for: AVMediaType.video)
+        let videoDevices = deviceDiscoverySession.devices
+        var captureDevice:AVCaptureDevice?
+        for device in videoDevices
+        {
+            if(device.position == AVCaptureDevice.Position.back)
+            {
+                captureDevice = device
+                break
+            }
+        }
+        if(captureDevice != nil)
+        {
+            captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        }
+        return captureDevice!
+    }
+    
+    func getVideoSession() -> AVCaptureSession
+    {
+        return m_videoCaptureSession
+    }
+    
+    //AVCaptureVideoDataOutputSampleBufferDelegate
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection)
+    {
+        if(inputDevice == nil)
+        {
+            return
+        }
+        
+        if(inputDevice?.isAdjustingFocus == false)
+        {
+            itrFocusFinish = itrFocusFinish + 1
+            if(itrFocusFinish == 1)
+            {
+                firstFocusFinish = true
+            }
+        }
+        if(!firstFocusFinish || isPauseFramesComing == true || isCurrentFrameDecodeFinished == false)
+        {
+            return
+        }
+
+        isCurrentFrameDecodeFinished = false
+        let imageBuffer:CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        CVPixelBufferLockBaseAddress(imageBuffer, .readOnly)
+        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
+        let bufferSize = CVPixelBufferGetDataSize(imageBuffer)
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        let bpr = CVPixelBufferGetBytesPerRow(imageBuffer)
+        CVPixelBufferUnlockBaseAddress(imageBuffer, .readOnly)
+        startRecognitionDate = NSDate()
+        let buffer = Data(bytes: baseAddress!, count: bufferSize)
+        guard let results = try? barcodeReader.decodeBuffer(buffer, withWidth: width, height: height, stride: bpr, format: .ARGB_8888, templateName: "") else { return }
+        DispatchQueue.main.async{
+            self.m_recognitionReceiver?.perform(self.m_recognitionCallback!, with: results as NSArray)
+        }
+    }
+    
+    func setRecognitionCallback(sender:ViewController, callBack:Selector)
+    {
+        m_recognitionReceiver = sender
+        m_recognitionCallback = callBack
+    }
+    
+    func setServerLicenseVerificationCallback(sender:ViewController, callBack:Selector)
+    {
+        m_verificationReceiver = sender
+        m_verificationCallback = callBack
+    }
+    
+    /*func ltsLicenseVerificationCallback(_ isSuccess: Bool, error: Error?) {
+        let boolNumber = NSNumber(value: isSuccess)
+        DispatchQueue.main.async{
+            self.m_verificationReceiver?.perform(self.m_verificationCallback!, with: boolNumber, with: error)
+        }
+    }*/
+    
+    func dlsLicenseVerificationCallback(_ isSuccess: Bool, error: Error?) {
+        let boolNumber = NSNumber(value: isSuccess)
+        DispatchQueue.main.async{
+            self.m_verificationReceiver?.perform(self.m_verificationCallback!, with: boolNumber, with: error)
+        }
+    }
+}
---
+++ ios/Swift/DBRwithoutDCE/DBRwithoutDCE/ViewController.swift
@@ -0,0 +1,361 @@
+//
+//  ViewController.swift
+//  DBRwithoutDCE
+//
+//  Created by Amro Ibrahim on 2022-02-23.
+//
+
+import UIKit
+import AVFoundation
+import DynamsoftBarcodeReader
+
+class ViewController: UIViewController {
+    @IBOutlet var rectLayerImage: UIImageView!
+    @IBOutlet weak var flashButton: UIButton!
+    @IBOutlet weak var detectDescLabel: UILabel!
+    
+    var cameraPreview: UIView?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var dbrManager: DbrManager?
+    var isFlashOn:Bool!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let dls = iDMDLSConnectionParameters()
+        // The organization id 200001 here will grant you a public trial license good for 7 days. After that, you can send an email to trial@dynamsoft.com (make sure to include the keyword privateTrial in the email title) to obtain a 30-day free private trial license which will also come in the form of an organization id.
+        dls.organizationID = "200001"
+        dbrManager = DbrManager(DLS: dls)
+        
+        dbrManager?.setServerLicenseVerificationCallback(sender: self, callBack: #selector(onVerificationCallBack(isSuccess:error:)))
+        dbrManager?.setRecognitionCallback(sender: self, callBack: #selector(onReadImageBufferComplete))
+        dbrManager?.setVideoSession()
+        
+        self.configInterface()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        dbrManager?.isPauseFramesComing = false
+        self.turnFlashOn(on: isFlashOn)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "showBarcodeTypes"){
+            let newBackButton = UIBarButtonItem.init(title: "", style: UIBarButtonItem.Style.plain, target: nil, action: nil)
+            self.navigationItem.backBarButtonItem = newBackButton
+            self.navigationController?.navigationBar.tintColor = UIColor.white
+            //let destViewController = segue.destination as! BarcodeTypesTableViewController
+            //destViewController.mainView = self
+            
+            self.turnFlashOn(on: false)
+            dbrManager?.isPauseFramesComing = true
+        }
+    }
+    
+    @objc func onVerificationCallBack(isSuccess:NSNumber, error:NSError?)
+    {
+        if(isSuccess.boolValue)
+        {
+            self.dbrManager?.startVideoSession()
+        }
+        else
+        {
+            var msg:String? = nil
+            var title = "Server license verify failed"
+            var add:Bool = true
+            if(error != nil)
+            {
+                if error?.code == -1009 {
+                    msg = "Dynamsoft Barcode Reader is unable to connect to the public Internet to acquire a license. Please connect your device to the Internet or contact support@dynamsoft.com to acquire an offline license."
+                    add = false
+                    title = "No Internet"
+                }else{
+                    msg = error!.userInfo[NSUnderlyingErrorKey] as? String
+                    if(msg == nil)
+                    {
+                        msg = error?.localizedDescription
+                    }
+                }
+            }
+            let ac = UIAlertController(title: title, message: msg,preferredStyle: .alert)
+            self.customizeAC(ac:ac)
+            let okButton = UIAlertAction(title: "Try again", style: .default, handler: {
+                action in
+                let dls = iDMDLSConnectionParameters()
+                dls.organizationID = "200001"
+                self.dbrManager?.connectServerAfterInit(DLS: dls)
+            })
+            ac.addAction(okButton)
+            if add {
+                let cancelButton = UIAlertAction(title: "Continue Anyway", style: .cancel, handler: {
+                    action in
+                    self.dbrManager?.startVideoSession()
+                })
+                ac.addAction(cancelButton)
+            }
+            self.present(ac, animated: false, completion: nil)
+        }
+    }
+    
+    @objc func onReadImageBufferComplete(readResult:NSArray)
+    {
+        let results:[iTextResult]? = readResult as? [iTextResult]
+        if(results!.count == 0 || dbrManager?.isPauseFramesComing == true)
+        {
+            dbrManager?.isCurrentFrameDecodeFinished = true
+            return
+        }
+        let timeInterval = (dbrManager?.startRecognitionDate?.timeIntervalSinceNow)! * -1
+        var msgText:String = ""
+        var hasEx:Bool = false
+        if(results!.count == 0){
+            dbrManager?.isCurrentFrameDecodeFinished = true
+            return
+        }else{
+            for item in results! {
+                if item.exception != nil && item.exception != "" {
+                    let msg = "Email trial@dynamsoft.com to acquire a 30-day trial license, make sure to include the word \"privateTrial\" in the email title."
+                    if item.exception!.contains(msg) {
+                        hasEx = true
+                        msgText = item.exception!
+                        let index:Range = msgText.range(of: "message:")!
+                        msgText = String(msgText[index.upperBound..<msgText.endIndex])
+                        break
+                    }
+                }
+                if item.barcodeFormat_2.rawValue != 0 {
+                    msgText = msgText + String(format:"\nType: %@\nValue: %@\n", item.barcodeFormatString_2!, item.barcodeText ?? "noResuslt")
+                }else{
+                    msgText = msgText + String(format:"\nType: %@\nValue: %@\n", item.barcodeFormatString!,item.barcodeText ?? "noResuslt")
+                }
+            }
+            var msg = ""
+            var title = ""
+            if (hasEx) {
+                msg = msgText
+                title = "Exception"
+            }else{
+                msg = msgText + String(format:"\nInterval: %.03f seconds",timeInterval)
+                title = "Result"
+            }
+            let ac = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+            self.customizeAC(ac:ac)
+            let okButton = UIAlertAction(title: "OK", style: .default, handler: {
+                action in
+                self.dbrManager?.isCurrentFrameDecodeFinished = true
+                self.dbrManager?.startVidioStreamDate! = NSDate()
+            })
+            ac.addAction(okButton)
+            self.present(ac, animated: false, completion: nil)
+        }
+    }
+    
+    @objc func didBecomeActive(notification:NSNotification) {
+        if(dbrManager?.isPauseFramesComing == false)
+        {
+            self.turnFlashOn(on: isFlashOn)
+        }
+    }
+    
+    func configInterface()
+    {
+        UIApplication.shared.isIdleTimerDisabled = true
+        NotificationCenter.default.addObserver(self, selector: #selector(didBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+        let w = UIScreen.main.bounds.size.width
+        let h = UIScreen.main.bounds.size.height
+        var mainScreenLandscapeBoundary = CGRect.zero
+        mainScreenLandscapeBoundary.size.width = min(w, h)
+        mainScreenLandscapeBoundary.size.height = max(w, h)
+        rectLayerImage?.frame = mainScreenLandscapeBoundary
+        rectLayerImage?.contentMode = UIView.ContentMode.topLeft
+        self.createRectBorderAndAlignControls()
+        isFlashOn = false
+        flashButton.layer.zPosition = 1
+        detectDescLabel.layer.zPosition = 1
+        flashButton.setTitle(" Flash off", for: UIControl.State.normal)
+        let captureSession = dbrManager?.getVideoSession()
+        if(captureSession == nil)
+        {
+            return
+        }
+        previewLayer = AVCaptureVideoPreviewLayer(session:captureSession!)
+        previewLayer!.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer!.frame = mainScreenLandscapeBoundary
+        cameraPreview = UIView()
+        cameraPreview!.layer.addSublayer(previewLayer!)
+        self.view.insertSubview(cameraPreview!, at: 0)
+    }
+    
+    func createRectBorderAndAlignControls()
+    {
+        let width = rectLayerImage.bounds.size.width
+        let height = rectLayerImage.bounds.size.height
+        let widthMargin = width * 0.1
+        let heightMargin = (height - width + 2 * widthMargin) / 2
+        UIGraphicsBeginImageContext(self.rectLayerImage.bounds.size)
+        let ctx = UIGraphicsGetCurrentContext()
+        //1. draw gray rect
+        UIColor.black.setFill()
+        ctx!.fill(CGRect(x: 0, y: 0, width: widthMargin, height: height))
+        ctx!.fill(CGRect(x: 0, y: 0, width: width, height: heightMargin))
+        ctx!.fill(CGRect(x: width - widthMargin, y: 0, width: widthMargin, height: height))
+        ctx!.fill(CGRect(x: 0, y: height - heightMargin, width: width, height: heightMargin))
+        //2. draw red line
+        var points = [CGPoint](repeating:CGPoint.zero, count: 2)
+        //3. draw white rect
+        UIColor.white.setStroke()
+        ctx!.setLineWidth(1.0)
+        // draw left side
+        points[0] = CGPoint(x:widthMargin,y:heightMargin)
+        points[1] = CGPoint(x:widthMargin,y:height - heightMargin)
+        ctx!.strokeLineSegments(between: points)
+        // draw right side
+        points[0] = CGPoint(x:width - widthMargin,y:heightMargin)
+        points[1] = CGPoint(x:width - widthMargin,y:height - heightMargin)
+        ctx!.strokeLineSegments(between: points)
+        // draw top side
+        points[0] = CGPoint(x:widthMargin,y:heightMargin)
+        points[1] = CGPoint(x:width - widthMargin,y:heightMargin)
+        ctx!.strokeLineSegments(between: points)
+        // draw bottom side
+        points[0] = CGPoint(x:widthMargin,y:height - heightMargin)
+        points[1] = CGPoint(x:width - widthMargin,y:height - heightMargin)
+        ctx!.strokeLineSegments(between: points)
+        //4. draw orange corners
+        UIColor.orange.setStroke()
+        ctx!.setLineWidth(2.0)
+        // draw left up corner
+        points[0] = CGPoint(x:widthMargin - 2,y:heightMargin - 2)
+        points[1] = CGPoint(x:widthMargin + 18,y:heightMargin - 2)
+        ctx!.strokeLineSegments(between: points)
+        points[0] = CGPoint(x:widthMargin - 2,y:heightMargin - 2)
+        points[1] = CGPoint(x:widthMargin - 2,y:heightMargin + 18)
+        ctx!.strokeLineSegments(between: points)
+        // draw left bottom corner
+        points[0] = CGPoint(x:widthMargin - 2,y:height - heightMargin + 2)
+        points[1] = CGPoint(x:widthMargin + 18,y:height - heightMargin + 2)
+        ctx!.strokeLineSegments(between: points)
+        points[0] = CGPoint(x:widthMargin - 2,y:height - heightMargin + 2)
+        points[1] = CGPoint(x:widthMargin - 2,y:height - heightMargin - 18)
+        ctx!.strokeLineSegments(between: points)
+        // draw right up corner
+        points[0] = CGPoint(x:width - widthMargin + 2,y:heightMargin - 2)
+        points[1] = CGPoint(x:width - widthMargin - 18,y:heightMargin - 2)
+        ctx!.strokeLineSegments(between: points)
+        points[0] = CGPoint(x:width - widthMargin + 2,y:heightMargin - 2)
+        points[1] = CGPoint(x:width - widthMargin + 2,y:heightMargin + 18)
+        ctx!.strokeLineSegments(between: points)
+        // draw right bottom corner
+        points[0] = CGPoint(x:width - widthMargin + 2,y:height - heightMargin + 2)
+        points[1] = CGPoint(x:width - widthMargin - 18,y:height - heightMargin + 2)
+        ctx!.strokeLineSegments(between: points)
+        points[0] = CGPoint(x:width - widthMargin + 2,y:height - heightMargin + 2)
+        points[1] = CGPoint(x:width - widthMargin + 2,y:height - heightMargin - 18)
+        ctx!.strokeLineSegments(between: points)
+        //5. set image
+        rectLayerImage.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        //6. align detectDescLabel horizontal center
+        var tempFrame = detectDescLabel.frame
+        tempFrame.origin.x = (width - detectDescLabel.bounds.size.width) / 2
+        tempFrame.origin.y = heightMargin * 0.6
+        detectDescLabel.frame = tempFrame
+        //7. align flashButton horizontal center
+        tempFrame = flashButton.frame
+        tempFrame.origin.x = (width - flashButton.bounds.size.width) / 2
+        tempFrame.origin.y = (heightMargin + (width - widthMargin * 2) + height) * 0.5 - flashButton.bounds.size.height * 0.5
+        flashButton.frame = tempFrame
+        return
+    }
+    
+    func turnFlashOn(on: Bool){
+        do
+        {
+            let device = AVCaptureDevice.default(for: AVMediaType.video)
+            if (device != nil && device!.hasTorch){
+                try device!.lockForConfiguration()
+                if (on == true) {
+                    device!.torchMode = AVCaptureDevice.TorchMode.on
+                    flashButton.setImage(UIImage(named: "flash_on"), for: UIControl.State.normal)
+                    flashButton.setTitle(" Flash on", for: UIControl.State.normal)
+                } else {
+                    device?.torchMode = AVCaptureDevice.TorchMode.off
+                    flashButton.setImage(UIImage(named: "flash_off"), for: UIControl.State.normal)
+                    flashButton.setTitle(" Flash off", for: UIControl.State.normal)
+                }
+                device?.unlockForConfiguration()
+            }
+        
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    func customizeAC(ac: UIAlertController){
+        
+        let subView1 = ac.view.subviews[0] as UIView
+        let subView2 = subView1.subviews[0] as UIView
+        let subView3 = subView2.subviews[0] as UIView
+        let subView4 = subView3.subviews[0] as UIView
+        let subView5 = subView4.subviews[0] as UIView
+        
+        for i in 0 ..< subView5.subviews.count
+        {
+            if(subView5.subviews[i].isKind(of: UILabel.self))
+            {
+                let label = subView5.subviews[i] as! UILabel
+                label.textAlignment = NSTextAlignment.left
+            }
+        }
+    }
+    
+    @IBAction func onFlashButtonClick(_ sender: Any) {
+        isFlashOn = isFlashOn == true ? false : true
+        self.turnFlashOn(on: isFlashOn)
+    }
+    
+    @IBAction func onAboutInfoClick(_ sender: Any) {
+        dbrManager?.isPauseFramesComing = true
+        let ac = UIAlertController(title: "About", message: "\nDynamsoft Barcode Reader Mobile App Demo(Dynamsoft Barcode Reader SDK)\n\n© 2021 Dynamsoft. All rights reserved. \n\nIntegrate Barcode Reader Functionality into Your own Mobile App? \n\nClick 'Overview' button for further info.\n\n",preferredStyle: .alert)
+        self.customizeAC(ac:ac)
+        let linkAction = UIAlertAction(title: "Overview", style: .default, handler: {
+            action in
+            let urlString = "http://www.dynamsoft.com/Products/barcode-scanner-sdk-iOS.aspx"
+            let url = NSURL(string: urlString )
+            if(UIApplication.shared.canOpenURL(url! as URL))
+            {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url! as URL)
+                } else {
+                    UIApplication.shared.openURL(url! as URL)
+                }
+            }
+            self.dbrManager?.isPauseFramesComing = false
+        })
+        ac.addAction(linkAction)
+        
+        let yesButton = UIAlertAction(title: "OK", style: .default, handler: {
+            action in
+            self.dbrManager?.isPauseFramesComing = false
+        })
+        ac.addAction(yesButton)
+        self.present(ac, animated: true, completion: nil)
+    }
+}
+
+
