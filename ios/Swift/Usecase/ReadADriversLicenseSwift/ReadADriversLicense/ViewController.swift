a346b5a8e32969ea6ca377b18be28a44efd46970
arckongem <arckongem@gmail.com>
2022-05-16 16:00:33 +0800

    update sample

--- ios/Swift/Usecase/ReadADriversLicenseSwift/ReadADriversLicense/ViewController.swift
+++ ios/Swift/Usecase/ReadADriversLicenseSwift/ReadADriversLicense/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController, DBRTextResultListener, CompleteDelegate 
     var dce:DynamsoftCameraEnhancer! = nil
     var dceView:DCECameraView! = nil
     var alertView:DBRPopDrivingLicenseView! = nil
-    
+    var resultView:UITextView!
     var barcodeReader:DynamsoftBarcodeReader! = nil
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +29,8 @@ class ViewController: UIViewController, DBRTextResultListener, CompleteDelegate 
         
         //Create a camera module for video barcode scanning. In this section Dynamsoft Camera Enhancer (DCE) will handle the camera settings.
         configurationDCE()
+        
+        addResultView()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -52,18 +54,40 @@ class ViewController: UIViewController, DBRTextResultListener, CompleteDelegate 
     
         barcodeReader.setCameraEnhancer(dce)
         barcodeReader.setDBRTextResultListener(self)
+        barcodeReader.startScanning()
+    }
+    
+    func addResultView(){
+        let viewHeight:CGFloat = 300
+        resultView = UITextView(frame: CGRect(x: 0, y: FullScreenSizeHeight  - SafeAreaBottomHeight - viewHeight , width: self.view.frame.width, height: viewHeight ))
+        resultView.layer.borderColor = UIColor.white.cgColor
+        resultView.layer.borderWidth = 1.0
+        resultView.layer.cornerRadius = 12.0
+        resultView.layer.backgroundColor = UIColor.clear.cgColor
+        resultView.layoutManager.allowsNonContiguousLayout = false
+        resultView.isEditable = false
+        resultView.isSelectable = false
+        resultView.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        resultView.textColor = UIColor.white
+        resultView.textAlignment = .center
+        resultView.isHidden = true
+        self.view.addSubview(resultView)
     }
     
     // Get the TestResult object from the callback
     func textResultCallback(_ frameId: Int, imageData: iImageData, results: [iTextResult]?) {
         if results!.count > 0 {
             var isDriverLicense:Bool = false
-            if results!.first!.barcodeFormatString == "PDF417"{
+            let dropfirst:Substring = results!.first!.barcodeText!.dropFirst(4)
+            let prefix = dropfirst.prefix(15)
+            let subString = prefix.dropFirst(5)
+            let rules = NSPredicate(format: "SELF MATCHES %@", "^[0-9]{10}$")
+            let isMatch:Bool = rules.evaluate(with: subString)
+            let preMatch:Bool = prefix.hasPrefix("AAMVA") || prefix.hasPrefix("ANSI ")
+            if isMatch && preMatch{
                 isDriverLicense = true
-            }else{
-                isDriverLicense = false
             }
-            
+
             if isDriverLicense {
                 var type:[String] = []
                 var typeDes:[String] = []
@@ -83,18 +107,23 @@ class ViewController: UIViewController, DBRTextResultListener, CompleteDelegate 
                 }
                 
             }else{
-                var msgText:String = ""
-                let title:String = "Results"
+                var msgText:String = "Fail to extract the driver's info.The text of the barcode is:"
                 if results!.first!.barcodeFormat_2.rawValue != 0 {
                     msgText = msgText + String(format:"\nFormat: %@\nText: %@\n", results!.first!.barcodeFormatString_2!, results!.first!.barcodeText ?? "noResuslt")
                 }else{
                     msgText = msgText + String(format:"\nFormat: %@\nText: %@\n", results!.first!.barcodeFormatString!, results!.first!.barcodeText ?? "noResuslt")
                 }
-                showResult(title, msgText, "OK") {
+                DispatchQueue.main.async{
+                    self.resultView.isHidden = false
+                    self.resultView.text = msgText
                 }
             }
             
         }else{
+            DispatchQueue.main.async{
+                self.resultView.isHidden = true
+                self.resultView.text = nil
+            }
             return
         }
     }
