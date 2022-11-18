175ea5aa6a910ee30d003307641287356eed0685
Elemen Zhang <elemen@dynamsoft.com>
2022-11-18 10:41:20 +0800

    feat: refactor the way showing results to alert a dialog

--- android/HelloWorld/app/src/main/java/com/dynamsoft/helloworld/MainActivity.java
+++ android/HelloWorld/app/src/main/java/com/dynamsoft/helloworld/MainActivity.java
@@ -2,120 +2,154 @@
 
 package com.dynamsoft.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.dynamsoft.dbr.BarcodeReader;
-import com.dynamsoft.dbr.DBRLicenseVerificationListener;
-import com.dynamsoft.dce.DCECameraView;
-import com.dynamsoft.dce.CameraEnhancer;
-import com.dynamsoft.dce.CameraEnhancerException;
 import com.dynamsoft.dbr.BarcodeReaderException;
+import com.dynamsoft.dbr.DBRLicenseVerificationListener;
 import com.dynamsoft.dbr.ImageData;
 import com.dynamsoft.dbr.TextResult;
 import com.dynamsoft.dbr.TextResultListener;
+import com.dynamsoft.dce.CameraEnhancer;
+import com.dynamsoft.dce.CameraEnhancerException;
+import com.dynamsoft.dce.DCECameraView;
+import com.dynamsoft.dce.DCEFeedback;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author dynamsoft
+ */
 public class MainActivity extends AppCompatActivity {
-    BarcodeReader mReader;
-    CameraEnhancer mCameraEnhancer;
-    TextView tvRes;
+	BarcodeReader mReader;
+	CameraEnhancer mCameraEnhancer;
+	TextView tvRes;
+	private AlertDialog alertDialog;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        // Initialize license for Dynamsoft Barcode Reader.
-        // The license string here is a time-limited trial license. Note that network connection is required for this license to work.
-        // You can also request an extension for your trial license in the customer portal: https://www.dynamsoft.com/customer/license/trialLicense?product=dbr&utm_source=installer&package=android
-        BarcodeReader.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", new DBRLicenseVerificationListener() {
-            @Override
-            public void DBRLicenseVerificationCallback(boolean isSuccessful, Exception e) {
-                runOnUiThread(() -> {
-                    if (!isSuccessful) {
-                        e.printStackTrace();
-                        showErrorDialog(e.getMessage());
-                    }
-                });
-            }
-        });
+		// Initialize license for Dynamsoft Barcode Reader.
+		// The license string here is a time-limited trial license. Note that network connection is required for this license to work.
+		// You can also request an extension for your trial license in the customer portal: https://www.dynamsoft.com/customer/license/trialLicense?product=dbr&utm_source=installer&package=android
+		BarcodeReader.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", new DBRLicenseVerificationListener() {
+			@Override
+			public void DBRLicenseVerificationCallback(boolean isSuccessful, Exception e) {
+				runOnUiThread(() -> {
+					if (!isSuccessful) {
+						e.printStackTrace();
+						showDialog(String.valueOf(R.string.error_dialog_title), e.getMessage(), null);
+					}
+				});
+			}
+		});
 
-        // Add camera view for previewing video.
-        DCECameraView cameraView = findViewById(R.id.cameraView);
+		// Add camera view for previewing video.
+		DCECameraView cameraView = findViewById(R.id.cameraView);
 
-        // Create an instance of Dynamsoft Camera Enhancer for video streaming.
-        mCameraEnhancer = new CameraEnhancer(MainActivity.this);
-        mCameraEnhancer.setCameraView(cameraView);
+		// Create an instance of Dynamsoft Camera Enhancer for video streaming.
+		mCameraEnhancer = new CameraEnhancer(MainActivity.this);
+		mCameraEnhancer.setCameraView(cameraView);
 
-        try {
-            // Create an instance of Dynamsoft Barcode Reader.
-            mReader = new BarcodeReader();
-        } catch (BarcodeReaderException e) {
-            e.printStackTrace();
-        }
+		try {
+			// Create an instance of Dynamsoft Barcode Reader.
+			mReader = new BarcodeReader();
+		} catch (BarcodeReaderException e) {
+			e.printStackTrace();
+		}
 
-        // Bind the Camera Enhancer instance to the Barcode Reader instance to get frames from camera.
-        mReader.setCameraEnhancer(mCameraEnhancer);
+		// Bind the Camera Enhancer instance to the Barcode Reader instance to get frames from camera.
+		mReader.setCameraEnhancer(mCameraEnhancer);
 
-        // Register a listener to obtain the recognized barcode results.
-        mReader.setTextResultListener(new TextResultListener() {
-            // Obtain the recognized barcode results and display.
-            @Override
-            public void textResultCallback(int id, ImageData imageData, TextResult[] textResults) {
-                runOnUiThread(() -> showResult(textResults));
-            }
-        });
+//		try {
+//			PublicRuntimeSettings settings = mReader.getRuntimeSettings();
+//			settings.barcodeFormatIds = EnumBarcodeFormat.BF_ONED |
+//					EnumBarcodeFormat.BF_QR_CODE |
+//					EnumBarcodeFormat.BF_PDF417 |
+//					EnumBarcodeFormat.BF_DATAMATRIX;
+//		} catch (BarcodeReaderException e) {
+//			e.printStackTrace();
+//		}
 
-        // Add TextView to display recognized barcode results.
-        tvRes = findViewById(R.id.tv_res);
-    }
+		// Register a listener to obtain the recognized barcode results.
+		mReader.setTextResultListener(new TextResultListener() {
+			// Obtain the recognized barcode results and display.
+			@Override
+			public void textResultCallback(int id, ImageData imageData, TextResult[] textResults) {
+				runOnUiThread(() -> showResult(textResults));
+			}
+		});
 
-    @Override
-    public void onResume() {
-        // Start video barcode reading
-        try {
-            mCameraEnhancer.open();
-        } catch (CameraEnhancerException e) {
-            e.printStackTrace();
-        }
-        mReader.startScanning();
-        super.onResume();
-    }
+		// Add TextView to display recognized barcode results.
+		tvRes = findViewById(R.id.tv_res);
+	}
 
-    @Override
-    public void onPause() {
-        // Stop video barcode reading
-        try {
-            mCameraEnhancer.close();
-        } catch (CameraEnhancerException e) {
-            e.printStackTrace();
-        }
-        mReader.stopScanning();
-        super.onPause();
-    }
+	@Override
+	public void onResume() {
+		// Start video barcode reading
+		try {
+			mCameraEnhancer.open();
+		} catch (CameraEnhancerException e) {
+			e.printStackTrace();
+		}
+		mReader.startScanning();
+		super.onResume();
+	}
 
+	@Override
+	public void onPause() {
+		// Stop video barcode reading
+		try {
+			mCameraEnhancer.close();
+		} catch (CameraEnhancerException e) {
+			e.printStackTrace();
+		}
+		mReader.stopScanning();
+		super.onPause();
+	}
 
-    private void showResult(TextResult[] results) {
-        if (results != null && results.length > 0) {
-            String strRes = "";
-            for (int i = 0; i < results.length; i++)
-                strRes += results[i].barcodeText + "\n\n";
-            tvRes.setText(strRes);
-        } else {
-            tvRes.setText("");
-        }
-    }
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if (alertDialog != null) {
+			alertDialog.dismiss();
+		}
+	}
 
-    private void showErrorDialog(String message) {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle(R.string.error_dialog_title)
-                .setPositiveButton("OK",null)
-                .setMessage(message)
-                .show();
+	private void showResult(TextResult[] results) {
+		String strRes = "";
 
-    }
+		if (results != null && results.length > 0) {
+
+			DCEFeedback.vibrate(this);
+			mReader.stopScanning();
+
+			for (int i = 0; i < results.length; i++)
+				strRes += results[i].barcodeText + "\n\n";
+			if (alertDialog != null && alertDialog.isShowing()) {
+				return;
+			}
+			showDialog("Result", strRes, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					mReader.startScanning();
+				}
+			});
+		}
+	}
+
+	private void showDialog(String title, String message, DialogInterface.OnClickListener listener) {
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		alertDialog = dialog.setTitle(title)
+				.setPositiveButton("OK", listener)
+				.setMessage(message)
+				.setCancelable(false)
+				.show();
+	}
 
 }
